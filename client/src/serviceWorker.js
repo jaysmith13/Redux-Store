e3532a252bef48dcf833e4fdd629107a808f5a37
jaysmith13 <janeasmith13@gmail.com>
2022-09-01 00:31:11 -0600

    Update serviceWorker.js
    tried adding a boolean but everything turned red. deleted boolean and it's green again. should read documentation on service worker.

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -71,23 +71,31 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function checkValidServiceWorker(swUrl, config) {
+fetch(swUrl, {
+    headers: { 'Service-Worker': 'script' },
+  })
+    .then(response => {
+         const contentType = response.headers.get('content-type');
+ if (
+   response.status === 404 ||
+   (contentType != null && contentType.indexOf('javascript') === -1)
+ ) {
+ navigator.serviceWorker.ready.then(registration => {
+    registration.unregister().then(() => {
+      window.location.reload();
+    });
+  });
+} else {
+   registerValidSW(swUrl, config);
+}
+})
+.catch(() => {
+console.log(
+  'No internet connection found. App is running in offline mode.'
+);
+});
+}
 
 
 
