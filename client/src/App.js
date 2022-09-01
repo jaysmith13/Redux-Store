e59468a1782f518a630cee2024c13571465adab6
jaysmith13 <janeasmith13@gmail.com>
2022-09-01 00:18:10 -0600

    Update App.js
    correcting red

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,49 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import OrderHistory from "./pages/OrderHistory";
 import Nav from "./components/Nav";
-import Success from "./pages/purchase";
+import purchase from "./pages/purchase";
 
+import { Provider } from 'react-redux';
+import store from './utils/store';
+import purchase from "./pages/purchase";
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+        <Provider store={store}>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/orderHistory" component={OrderHistory} />
+            <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/purchase" component={purchase} />
+            <Route component={NoMatch} />
+          </Switch>
+          </Provider>
+        </div>
+      </Router>
+    </ApolloProvider>
+
+  );
+}
+
+export default App;
 
 
 
