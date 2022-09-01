23716fa35571c330f96ca4352ff3478cea2b6b04
jaysmith13 <janeasmith13@gmail.com>
2022-08-31 23:59:00 -0600

    Update signup.js
    started signup

---
+++ client/src/pages/signup.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from '@apollo/react-hooks';
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
+
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
