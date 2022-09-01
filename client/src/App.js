009f220511f4d08a68bd01d909df5c2eea15e043
jaysmith13 <janeasmith13@gmail.com>
2022-09-01 00:11:23 -0600

    Update App.js
    adding more imports and the export

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
-
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import OrderHistory from "./pages/OrderHistory";
+import Nav from "./components/Nav";
+import Success from "./pages/purchase";
 
 
 
